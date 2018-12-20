b7b2c288682882a7fe99745474fec0c718745085
munHunger <marcusmunger@hotmail.com>
2018-12-20 15:58:36 +0100

    base of parser implemented

--- wiki/src/service/parser.js
+++ wiki/src/service/parser.js
@@ -1,5 +1,20 @@
-function parse(input) {
-  return "lame";
+function parse (input) {
+  return input.reduce(
+    (acc, val) => {
+      if (val.startsWith('@')) {
+        var re = /(?<=\[).*?(?=\])/gm
+        let list = []
+        var m
+        do {
+          m = re.exec(val)
+          if (m) list.push(m[0])
+        } while (m)
+        acc.info.push({ name: list[0], data: list.splice(1) })
+      } else acc.data += (acc.data.length > 0 ? '\n' : '') + val
+      return acc
+    },
+    { data: '', info: [] }
+  )
 }
 
-module.exports = { parse };
+module.exports = { parse }
--- wiki/src/service/parser.spec.js
+++ wiki/src/service/parser.spec.js
@@ -1,4 +1,19 @@
-const { parse } = require("./parser");
+const { parse } = require('./parser')
 
-describe("LOL OK", () =>
-  it("does shit", () => expect(parse(null)).toBe("lame")));
+describe('Parser', () => {
+  describe('With empty input', () =>
+    it('Produces an empty parsed object', () =>
+      expect(parse([])).toEqual({ data: '', info: [] })))
+  describe('Adding data', () => {
+    it('Adds plain data', () =>
+      expect(parse(['data', 'more data'])).toEqual({
+        data: 'data\nmore data',
+        info: []
+      }))
+    it('Ignores info tags', () =>
+      expect(parse(['@i[data][more data]', 'more data'])).toEqual({
+        data: 'more data',
+        info: [{ name: 'data', data: ['more data'] }]
+      }))
+  })
+})
