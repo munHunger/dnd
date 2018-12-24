87bceddcd55dde3519cf64a0a531c86b94b2f67a
munHunger <marcusmunger@hotmail.com>
2018-12-24 12:00:19 +0100

    cleanup

--- wiki/src/renderer/components/LandingPage.vue
+++ wiki/src/renderer/components/LandingPage.vue
@@ -1,21 +1,17 @@
 <template>
   <div id="wrapper">
-    <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
     <main>
-      <div class="left-side">
-        <post file-name="mary.md"></post>
-      </div>
+      <post file-name="mary.md"></post>
     </main>
   </div>
 </template>
 
 <script>
-import SystemInformation from "./LandingPage/SystemInformation";
 import Post from "./Post/Post";
 
 export default {
   name: "landing-page",
-  components: { SystemInformation, Post },
+  components: { Post },
   methods: {
     open(link) {
       this.$electron.shell.openExternal(link);
@@ -45,12 +41,6 @@ body {
   width: 100vw;
 }
 
-#logo {
-  height: auto;
-  margin-bottom: 20px;
-  width: 420px;
-}
-
 main {
   display: flex;
   justify-content: space-between;
@@ -58,52 +48,5 @@ main {
 
 main > div {
   flex-basis: 50%;
-}
-
-.left-side {
-  display: flex;
-  flex-direction: column;
-}
-
-.welcome {
-  color: #555;
-  font-size: 23px;
-  margin-bottom: 10px;
-}
-
-.title {
-  color: #2c3e50;
-  font-size: 20px;
-  font-weight: bold;
-  margin-bottom: 6px;
-}
-
-.title.alt {
-  font-size: 18px;
-  margin-bottom: 10px;
-}
-
-.doc p {
-  color: black;
-  margin-bottom: 10px;
-}
-
-.doc button {
-  font-size: 0.8em;
-  cursor: pointer;
-  outline: none;
-  padding: 0.75em 2em;
-  border-radius: 2em;
-  display: inline-block;
-  color: #fff;
-  background-color: #4fc08d;
-  transition: all 0.15s ease;
-  box-sizing: border-box;
-  border: 1px solid #4fc08d;
-}
-
-.doc button.alt {
-  color: #42b983;
-  background-color: transparent;
 }
 </style>
