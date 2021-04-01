b74c2c585b906b4dfb8085d962db19a126d14c8e
marmun <marcus.munger@kambi.com>
2021-04-01 16:29:33 +0200

    broken

--- dmScreen/src/lib/QuadTree.svelte
+++ dmScreen/src/lib/QuadTree.svelte
@@ -5,14 +5,15 @@
 	export let tree;
 	export let debug = false;
 	let canvas;
+	let canvasOverlay;
 	let rc;
-    let ctx;
+	let ctx;
 	let zoom = 50;
 	onMount(() => {
 		ctx = canvas.getContext('2d');
 		rc = rough.canvas(canvas);
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        zoom = canvas.width / tree.bounds.width
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		zoom = canvas.width / tree.bounds.width;
 		drawTree(tree, ctx, { zoom });
 	});
 	/**
@@ -65,10 +66,10 @@
 					{
 						fill: '#81A1C1',
 						fillStyle: 'zigzag',
-						fillStyle: 'solid',
+						//fillStyle: 'solid',
 						fillWeight: 8,
 						hachureGap: 10,
-						roughness: 2
+						roughness: 1
 					}
 				);
 
@@ -92,27 +93,73 @@
 		if (tree.br) drawTree(tree.br, ctx, options);
 	}
 
+	function toMapSpace(x, y) {
+		return [(x / 1000) * tree.bounds.width, (y / 1000) * tree.bounds.height];
+	}
+
+	let clickState = 0;
+	let start;
+
 	function onClick(e) {
+		clickState = (clickState + 1) % 2;
 		let bounds = quadTree.rect(
-			(e.offsetX / 1000) * tree.bounds.width,
-			(e.offsetY / 1000) * tree.bounds.height,
-			0.5,
-		    0.5
+			...[
+				...toMapSpace(start[0], start[1]),
+				...toMapSpace(e.offsetX - start[0], e.offsetY - start[1])
+			]
 		);
-        console.log(bounds)
-        let old = JSON.stringify(tree)
-		tree = quadTree.addRect(tree, {
-			bounds,
-			obj: { type: 'house' }
-		});
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-		drawTree(tree, ctx, {zoom});
-		console.log(tree);
-        console.log(JSON.stringify(tree) === old)
+		console.log(bounds);
+		let old = JSON.stringify(tree);
+		if (clickState === 0) {
+			tree = quadTree.addRect(tree, {
+				bounds,
+				obj: { type: 'house' }
+			});
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			drawTree(tree, ctx, { zoom });
+			console.log(tree);
+			console.log(JSON.stringify(tree) === old);
+			canvasOverlay.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+		} else {
+			start = [e.offsetX, e.offsetY];
+		}
+	}
+
+	function onMouseMove(e) {
+		console.log(e);
+		if (clickState === 1) {
+			canvasOverlay.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+			rough
+				.canvas(canvasOverlay)
+				.rectangle(start[0], start[1], e.offsetX - start[0], e.offsetY - start[1], {
+					fill: '#81A1C1',
+					fillStyle: 'zigzag',
+					//fillStyle: 'solid',
+					fillWeight: 8,
+					hachureGap: 10,
+					roughness: 1
+				});
+		}
 	}
 </script>
 
 {tree}
 quadTree
 
-<canvas bind:this={canvas} width="1000" height="1000" style="background-color: #f0f9ff;" on:click={onClick} />
+<canvas bind:this={canvas} width="1000" height="1000" style="background-color: #f0f9ff;" />
+
+<canvas
+	bind:this={canvasOverlay}
+	width="1000"
+	height="1000"
+	on:click={onClick}
+	on:mousemove={onMouseMove}
+/>
+
+<style>
+	canvas {
+		position: absolute;
+		top: 0px;
+		left: 0px;
+	}
+</style>
