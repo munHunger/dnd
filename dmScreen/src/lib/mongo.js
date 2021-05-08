8e2dcf3eb86ee59158edc89a15b7dc621ba21004
munhunger <marcusmunger@gmail.com>
2021-05-08 08:39:16 +0200

    fixing port issues

--- dmScreen/src/lib/mongo.js
+++ dmScreen/src/lib/mongo.js
@@ -1,17 +1,21 @@
 import MongoClient from 'mongodb';
 
+// Connection URL
+const url = process.env['MONGO_URL'] || 'mongodb://localhost:27017';
+console.log('mong url: ' + url);
+const user = process.env['MONGO_USER'] || 'root';
+const password = process.env['MONGO_PASSWORD'] || 'example';
+console.log(user);
+console.log(password);
 /**
  * @param {string} name
  * @returns {Promise<import('mongodb').Collection>}
  */
 export function collection(name) {
-	// Connection URL
-	const url = process.env.MONGO_URL || 'mongodb://localhost:27017';
-
 	// Database Name
 	const dbName = 'dmScreen';
 	return new Promise((resolve, reject) =>
-		MongoClient.connect(url, { auth: { user: 'root', password: 'example' } }).then((c) => {
+		MongoClient.connect(url, { auth: { user, password } }).then((c) => {
 			c.db(dbName).collection(name, (err, res) => {
 				if (err) return reject(err);
 				resolve(res);
