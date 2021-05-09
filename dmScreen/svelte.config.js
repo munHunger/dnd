e21636d3b1068c1446debde96a78e98fce24e3e4
munhunger <marcusmunger@gmail.com>
2021-05-09 19:26:11 +0200

    updated dependencies

--- dmScreen/svelte.config.js
+++ dmScreen/svelte.config.js
@@ -1,8 +1,7 @@
-const node = require('@sveltejs/adapter-node');
-const pkg = require('./package.json');
+import node from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
-module.exports = {
+export default {
 	kit: {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
@@ -10,12 +9,6 @@ module.exports = {
 		adapter: node(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-
-		vite: {
-			ssr: {
-				noExternal: Object.keys(pkg.dependencies || {})
-			}
-		}
+		target: '#svelte'
 	}
 };
