6cf62caf472552caec97fb965057b2e0024a11c9
munhunger <marcusmunger@gmail.com>
2021-05-09 11:10:02 +0200

    fixing logging

--- dmScreen/src/lib/mongo.js
+++ dmScreen/src/lib/mongo.js
@@ -1,12 +1,11 @@
 import MongoClient from 'mongodb';
+import { logger } from './logger';
 
 // Connection URL
 const url = process.env['MONGO_URL'] || 'mongodb://localhost:27017';
-console.log('mong url: ' + url);
+logger.info('initializing mongo url=' + url);
 const user = process.env['MONGO_USER'] || 'root';
 const password = process.env['MONGO_PASSWORD'] || 'example';
-console.log(user);
-console.log(password);
 /**
  * @param {string} name
  * @returns {Promise<import('mongodb').Collection>}
